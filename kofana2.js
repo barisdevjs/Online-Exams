8cf6ce098cc4161c3c03c8730bfef3e7846fd206
Barış SAVAŞ <barissavas17@gmail.com>
2022-04-18 18:16:59 +0300

    solved

---
+++ kofana2.js
@@ -0,0 +1,12 @@
+function StringChallenge(str) { 
+    let str2 = str.split(' ').map(word => {
+      return word.split('').map(char => {
+        return char === char.toUpperCase() ?
+        char.toLowerCase() : char.toUpperCase()
+      }).join('')
+    }).join(' ')
+    return str2
+    }
+       
+    // keep this function call here 
+    console.log(StringChallenge(readline()));
