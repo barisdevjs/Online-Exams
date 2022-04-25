3ca127ef5c44e6b3b223437a5009377b12539754
Barış SAVAŞ <barissavas17@gmail.com>
2022-04-25 23:29:14 +0300

    solved

---
+++ bayzat.js
@@ -0,0 +1,24 @@
+function ArrayChallenge(arr) { 
+    const myChallengeToken = 'd1vibqr5'
+    if ( arr.length < 2) return -1
+    let minPrice = arr[0]
+    let maxProfit = arr[1] - arr[0]
+    let minIdx = 0
+  
+    for ( let i = 1; i < arr.length; ++i) {
+      if (arr[i] - minPrice > maxProfit) {
+        maxProfit = arr[i] - minPrice
+      }
+  
+      if ( arr[i] < minPrice && i !== arr.length -1) {
+        minPrice = arr[i]
+        minIdx = i
+      }
+    }
+    
+    const final = maxProfit + myChallengeToken
+    const result = final.replace(/(..)./g, "$1X");
+    return result
+  }
+
+  console.log(ArrayChallenge([10,12,4,5,9]))
