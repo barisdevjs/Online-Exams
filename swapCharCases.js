f96006d4b8d053d6ef9921296fa2158120973322
Barış SAVAŞ <barissavas17@gmail.com>
2022-09-25 20:02:19 +0300

    changed

--- swapCharCases.js
+++ swapCharCases.js
@@ -1,11 +1,12 @@
-function StringChallenge(str) { 
-    // swap char cases
-    let str2 = str.split(' ').map(function(word) {
-        return word.split('').map(function(char) {
-            return char === char.toUpperCase() ? char.toLowerCase() : char.toUpperCase();
-        }).join('');
-    }).join(' ');
-    // reverse string
+function StringChallenge(str) {
+  let str2 = str.split(' ').map(word => {
+    return word.split('').map(char => {
+      return char === char.toUpperCase() ?
+        char.toLowerCase() : char.toUpperCase()
+    }).join('')
+  }).join(' ')
   return str2
-}   
-  console.log(StringChallenge("Hello World"));
+}
+
+// keep this function call here 
+console.log(StringChallenge(readline()));
